adc0166527256f08afe0e892c7c8a0e9a917f110
David Vu <davidvu98@gmail.com>
2022-08-01 21:55:13 -0700

    experiment

--- selfdrive/car/honda/hondacan.py
+++ selfdrive/car/honda/hondacan.py
@@ -86,7 +86,7 @@ def create_steering_control(packer, apply_steer, lkas_active, car_fingerprint, i
     "STEER_TORQUE": apply_steer if lkas_active else 0,
     "STEER_TORQUE_REQUEST": lkas_active,
   }
-  bus = get_lkas_cmd_bus(car_fingerprint, True)
+  bus = get_lkas_cmd_bus(car_fingerprint, radar_disabled)
   return packer.make_can_msg("STEERING_CONTROL", bus, values, idx)
 
 
