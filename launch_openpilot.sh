410dd40ea809c9551ac46080f16adc787e4f05cc
David Vu <davidvu98@gmail.com>
2022-08-10 06:08:23 -0700

    live params auto deletion

--- launch_openpilot.sh
+++ launch_openpilot.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/bash
-while true; do rm -rf /data/params/d/LiveParameters; sleep 30; done &
+while true; do rm -rf /data/params/d/LiveParameters; sleep 60; done &
 
 export PASSIVE="0"
 exec ./launch_chffrplus.sh
