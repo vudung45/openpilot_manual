61c5dbda234d1869a8067f6e35d693bf28b68e80
David Vu <davidvu98@gmail.com>
2022-08-17 03:37:43 -0700

    hybird radar/radar disabled

--- selfdrive/car/honda/carcontroller.py
+++ selfdrive/car/honda/carcontroller.py
@@ -153,7 +153,7 @@ class CarController:
     can_sends = []
 
     radar_disabed = (self.CP.carFingerprint in HONDA_BOSCH and self.CP.openpilotLongitudinalControl) or \
-    (self.CP.carFingerprint == CAR.ACURA_RDX_3G and CS.out.vEgo < ACURA_RDX_3G_MIN_STEER)
+    (self.CP.carFingerprint == CAR.ACURA_RDX_3G and CS.out.vEgo < ACURA_RDX_3G_MIN_STEER and apply_steer != 0 and CC.latActive)
     # tester present - w/ no response (keeps radar disabled)
     if radar_disabled:
       if self.frame % 10 == 0:
